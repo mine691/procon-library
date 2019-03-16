1660a8bf57997b91bb6f43a2769da46eb6f68b23
mine691 <48609592+mine691@users.noreply.github.com>
2019-03-16 11:57:33 +0900

    Update eratosthenes.cpp

--- eratosthenes.cpp
+++ eratosthenes.cpp
@@ -1,19 +1,20 @@
-vector<int> eratosthenes(int n)
+// order N*(log (log N))
+vector<int> eratosthenes(int N)
 {
     vector<int> res;
-    vector<bool> prime(n + 1, 1);
+    vector<bool> prime(N + 1, 1);
     prime[0] = prime[1] = false;
-    for (int i = 2; i * i <= n; ++i)
+    for (int i = 2; i * i <= N; ++i)
     {
         if (prime[i] == true)
         {
-            for (int j = 2 * i; j <= n; j += i)
+            for (int j = 2 * i; j <= N; j += i)
             {
                 prime[j] = false;
             }
         }
     }
-    for (int i = 0; i <= n; ++i)
+    for (int i = 0; i <= N; ++i)
     {
         if (prime[i] == true)
             res.push_back(i);
