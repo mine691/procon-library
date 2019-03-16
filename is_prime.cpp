cecd0fdbfa9e59bd6b6d12cff8a02d3fb9ed7bff
mine691 <48609592+mine691@users.noreply.github.com>
2019-03-16 11:56:28 +0900

    Update is_prime.cpp

--- is_prime.cpp
+++ is_prime.cpp
@@ -1,11 +1,12 @@
+//order root N
 template <typename T>
-bool is_prime(T n)
+bool is_prime(T N)
 {
-    if (n < 2)
+    if (N < 2)
         return false;
-    for (T i = 2; i * i <= n; ++i)
+    for (T i = 2; i * i <= N; ++i)
     {
-        if (n % i == 0)
+        if (N % i == 0)
             return false;
     }
     return true;
