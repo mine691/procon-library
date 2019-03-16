bfc84f5e59e479f8ab6e7efbf1437ff01c7916dc
mine691 <48609592+mine691@users.noreply.github.com>
2019-03-16 16:20:50 +0900

    Create pascal.cpp

---
+++ combinatorics/pascal.cpp
@@ -0,0 +1,22 @@
+/*
+pascalの三角形を求める
+計算量は 𝑂(𝑁𝑟),見積もりとしてはO(𝑁²) 
+N<3000くらいならOK
+*/
+
+#include <vector>
+using ll = long long;
+vector<vector<ll>> get_combination(int N)
+{
+    vector<vector<ll>> pascal(N + 1, vector<ll>(N + 1));
+    for (int i = 0; i <= N; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i)
+                pascal[i][j] = 1;
+            else
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+        }
+    }
+}
