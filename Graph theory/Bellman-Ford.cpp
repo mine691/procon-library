d5a473dc8bf2833dfb4e726e9018b521429a2a46
mine691 <48609592+mine691@users.noreply.github.com>
2019-05-30 23:39:53 +0900

    Update Bellman-Ford.cpp

--- Graph theory/Bellman-Ford.cpp
+++ Graph theory/Bellman-Ford.cpp
@@ -1,31 +1,25 @@
-struct edge
-{
-    int to, cost;
-};
-
 template <typename T>
-using Edges = vector<edge<T>>;
-
-template <typename T>
-vector<T> BellmanFord(Edges<T> &edges, int N, int start)
+vector<T> BellmanFrod(Edges<T> &edges, int N, int source)
 {
-    ll INF = 1e18;
-    vector<T> dist(N, INF);
-    dist[start] = 0;
+    const auto INF = numeric_limits<T>::max();
+    vector<T> dist(N, INF); //distance from source
+    dist[source] = 0;
     for (int i = 0; i < N - 1; i++)
     {
         for (auto &e : edges)
         {
-            if (dist[e.scr] == INF)
+            if (dist[e.src] == INF)
                 continue;
-            dist[e.to] = min(dist[e.to], dist[e.scr] + e.cost);
+            dist[e.to] = min(dist[e.to], dist[e.src] + e.cost);
         }
     }
+
+    // check for negative-weight cycles
     for (auto &e : edges)
     {
-        if (dist[e.scr] == INF)
+        if (dist[e.src] == INF)
             continue;
-        if (dist[e.scr] + e.cost < dist[e.to])
+        if (dist[e.src] + e.cost < dist[e.to])
             return vector<T>();
     }
     return dist;
