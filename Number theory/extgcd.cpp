f07e476507a0e4df1a34d2287fc7e2971cb17e47
mine691 <48609592+mine691@users.noreply.github.com>
2019-05-31 16:46:07 +0900

    Create extgcd.cpp

---
+++ Number theory/extgcd.cpp
@@ -0,0 +1,10 @@
+// g = gcd(a, b)
+// (x, y) satisfies ax + by = gcd(a, b)
+template <typename T>
+T extGCD(T a, T b, T &x, T &y)
+{
+    if (b == 0) { x = 1, y = 0; return a; }
+    T d = extGCD(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}
