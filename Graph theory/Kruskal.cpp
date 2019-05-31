e6711a60939e35c0fec67e8b98bc7a417f5f4b45
mine691 <48609592+mine691@users.noreply.github.com>
2019-05-31 20:50:23 +0900

    Create Kruskal.cpp

---
+++ Graph theory/Kruskal.cpp
@@ -0,0 +1,18 @@
+//need Union-find
+
+#include <algorithm>
+#include <vector>
+
+template <typename T>
+T Kruskal(Edges<T> &edges, int V)
+{
+    sort(edges.begin(), edges.end(), [](const edge<T> &a, const edge<T> &b) { return a.cost < b.cost; });
+    UnionFind<T> uf(V);
+    T res = 0;
+    for (auto &e : edges)
+    {
+        if (uf.unite(e.src, e.to) == true)
+            res += e.cost;
+    }
+    return res;
+}
