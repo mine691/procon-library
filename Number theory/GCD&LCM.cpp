fef7188fcce8713f59d5e32bd6e2e17764b5c016
mine691 <48609592+mine691@users.noreply.github.com>
2019-07-05 00:54:18 +0900

    Update GCD&LCM.cpp

--- Number theory/GCD&LCM.cpp
+++ Number theory/GCD&LCM.cpp
@@ -1,11 +1,2 @@
-template <typename T>
-T gcd(T a, T b)
-{
-    return (b == 0 ? a : gcd(b, a % b));
-}
-
-template <typename T>
-T lcm(T a, T b)
-{
-    return a / (gcd(a, b)) * b;
-}
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+ll lcm(ll a, ll b) { return a / (gcd(a, b)) * b; }
