8635e8adfef99671a69a4da3f394be5c6be7668a
mine691 <48609592+mine691@users.noreply.github.com>
2019-07-05 13:41:36 +0900

    Update combination_O(r).cpp

--- combinatorics/combination_O(r).cpp
+++ combinatorics/combination_O(r).cpp
@@ -1,18 +1,13 @@
-/*
-nCrの定義式から求める
-計算量 各クエリに対して𝑂(log 𝑟)
-*/
-
 using ll = long long;
-ll COM(ll n, ll k)
+ll comb(ll n, ll k)
 {
     if (k < 0 || n < k)
         return 0;
-    ll ret = 1;
+    ll res = 1;
     for (ll i = 1; i <= k; ++i)
     {
-        ret *= n--;
-        ret /= i;
+        res *= n--;
+        res /= i;
     }
-    return ret;
+    return res;
 }
