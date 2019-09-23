275d232c23d6901cb9cd18e37ee4d20fb1f6e794
mine691 <48609592+mine691@users.noreply.github.com>
2019-09-23 17:15:13 +0900

    Create motint.cpp

---
+++ Number theory/motint.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int mod = 1e9 + 7;
+const int inf = (1 << 30) - 1;
+const ll infll = (1LL << 61) - 1;
+#define fast() cin.tie(0), ios::sync_with_stdio(false)
+
+template <int mod>
+struct ModInt
+{
+    int x;
+
+    ModInt() : x(0) {}
+
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+    ModInt operator-() const { return ModInt(-x); }
+
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+    ModInt operator++() const { return ModInt(*this) += 1; }
+
+    ModInt operator--() const { return ModInt(*this) -= 1; }
+
+    bool operator==(const ModInt &p) const { return x == p.x; }
+
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+
+    ModInt &operator+=(const ModInt &p)
+    {
+        if ((x += p.x) >= mod)
+            x -= mod;
+        return *this;
+    }
+
+    ModInt &operator-=(const ModInt &p)
+    {
+        if ((x += mod - p.x) >= mod)
+            x -= mod;
+        return *this;
+    }
+
+    ModInt &operator*=(const ModInt &p)
+    {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+
+    ModInt &operator/=(const ModInt &p)
+    {
+        *this *= p.inverse();
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &os, const ModInt &p)
+    {
+        return os << p.x;
+    }
+
+    friend istream &operator>>(istream &is, ModInt &a)
+    {
+        int64_t t;
+        is >> t;
+        a = ModInt<mod>(t);
+        return (is);
+    }
+
+    ModInt pow(int64_t N) const
+    {
+        ModInt res(1), mult(x);
+        while (N > 0)
+        {
+            if (N & 1)
+                res *= mult;
+            mult *= mult;
+            N >>= 1;
+        }
+        return res;
+    }
+
+    ModInt inv() const
+    {
+        int a = x, b = mod, u = 1, v = 0, t;
+        while (b > 0)
+        {
+            t = a / b;
+            a -= t * b, u -= t * v;
+            swap(a, b), swap(u, v);
+        }
+        return ModInt(u);
+    }
+};
+
+using mint = ModInt<mod>;
