b1b2eb1f62edc0f6150f277a2bf0cd7bc155b67a
mine691 <48609592+mine691@users.noreply.github.com>
2020-03-27 21:26:04 +0900

    Update prime factorization.cpp

--- Number theory/prime factorization.cpp
+++ Number theory/prime factorization.cpp
@@ -1,4 +1,4 @@
-#include <map>
+// map
 template <typename T>
 map<T, int> prime_factor(T n)
 {
@@ -16,7 +16,7 @@ map<T, int> prime_factor(T n)
     return res;
 }
 
-#include <vector>
+// vector
 template <typename T>
 vector<T> prime_factor(T n)
 {
@@ -32,4 +32,60 @@ vector<T> prime_factor(T n)
     if (n != 1)
         res.push_back(n);
     return res;
+}
+
+//N以下の数について、それぞれの素因数を重複を除いて求める
+vector<vector<int>> prime_factor_lists(int N)
+{
+    vector<vector<int>> prime_lists(N + 1);
+    for (int i = 2; i <= N; i++)
+    {
+        if (prime_lists[i].size() == 0)
+        {
+            for (int j = i; j <= N; j += i)
+            {
+                prime_lists[j].push_back(i);
+            }
+        }
+    }
+    return prime_lists;
+}
+
+// for query using osa_k way
+
+// init table
+vector<int> sieve(int N)
+{
+    vector<int> res(N + 1);
+
+    for (int i = 2; i <= N; i++)
+    {
+        if (res[i] == 0)
+        {
+            for (int j = i; j <= N; j += i)
+            {
+                if (res[j] == 0)
+                    res[j] = i;
+            }
+        }
+    }
+    return res;
+}
+
+// prime factorization O(log N)
+vector<int> prime_factor(int N, const vector<int> &min_factor)
+{
+    // min_factor is vector which got by sieve()
+    vector<int> res;
+    while (N > 1)
+    {
+        res.push_back(min_factor[N]);
+        N /= min_factor[N];
+    }
+    return res;
+}
+
+bool is_prime(int N, const vector<int> &min_factor)
+{
+    return min_factor[N] == N;
 }
